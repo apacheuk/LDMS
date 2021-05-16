a75e73656538275cea066e0d0875cf84fc945910
simon <redmond.simonuk@gmail.com>
2021-05-16 10:39:25 +0100

    Re-ordered inserts to avoid integrity errors

--- setup.sql
+++ setup.sql
@@ -32,15 +32,15 @@ VALUES (90003, 'Roxy Jones', 'Salesperson',90002,'11-FEB-2017',35000,4);
 INSERT INTO employees_file(employee_id, employee_name, job_title, manager_id, date_hired, salary, department_id)
 VALUES (90004, 'Selwyn Field', 'Salesperson',90002,'20-MAY-2015',32000,4);
 INSERT INTO employees_file(employee_id, employee_name, job_title, manager_id, date_hired, salary, department_id)
-VALUES (90005, 'David Hallett', 'Engineer',90006,'17-APR-2018',40000,2);
-INSERT INTO employees_file(employee_id, employee_name, job_title, manager_id, date_hired, salary, department_id)
 VALUES (90006, 'Sarah Phelps ', 'Manager',90001,'21-MAR-2015',45000,2);
+INSERT INTO employees_file(employee_id, employee_name, job_title, manager_id, date_hired, salary, department_id)
+VALUES (90005, 'David Hallett', 'Engineer',90006,'17-APR-2018',40000,2);
 INSERT INTO employees_file(employee_id, employee_name, job_title, manager_id, date_hired, salary, department_id)
 VALUES (90007, 'Louise Harper', 'Engineer',90006,'01-JAN-2013',47000,2);
 INSERT INTO employees_file(employee_id, employee_name, job_title, manager_id, date_hired, salary, department_id)
-VALUES (90008, 'Tina Hart', 'Engineer',90009,'28-JUL-2014',45000,3);
-INSERT INTO employees_file(employee_id, employee_name, job_title, manager_id, date_hired, salary, department_id)
 VALUES (90009, 'Gus Jones', 'Manager',90001,'15-MAY-2018',50000,3);
+INSERT INTO employees_file(employee_id, employee_name, job_title, manager_id, date_hired, salary, department_id)
+VALUES (90008, 'Tina Hart', 'Engineer',90009,'28-JUL-2014',45000,3);
 INSERT INTO employees_file(employee_id, employee_name, job_title, manager_id, date_hired, salary, department_id)
 VALUES (90010, 'Mildred Hall', 'Secretary',90001,'12-OCT-1996',35000,1);
 
